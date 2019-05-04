25b10062a7436ba506d9946d813cd726c3d66766
Tejas Rana <tejasrana3898@gmail.com>
2019-05-04 06:29:21 +0530

    Add files via upload

--- Unittwo/app/src/main/java/com/android/tony/unittwo/MainActivity.java
+++ Unittwo/app/src/main/java/com/android/tony/unittwo/MainActivity.java
@@ -2,10 +2,12 @@ package com.android.tony.unittwo;
 
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
@@ -19,7 +21,9 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String myKey = "Remote Input";
+    public static final String myKey = "RemoteInput", myChannel = "Tony";
+    NotificationManager manager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,18 +31,19 @@ public class MainActivity extends AppCompatActivity {
 
         Button list = findViewById(R.id.listbutton);
         Button grid = findViewById(R.id.gridbutton);
+        manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
         list.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,ListViewActivity.class));
+                startActivity(new Intent(MainActivity.this, ListViewActivity.class));
             }
         });
 
         grid.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,GridViewActivity.class));
+                startActivity(new Intent(MainActivity.this, GridViewActivity.class));
             }
         });
 
@@ -46,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
         intent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,IntentActivity.class));
+                startActivity(new Intent(MainActivity.this, IntentActivity.class));
             }
         });
 
@@ -54,12 +59,12 @@ public class MainActivity extends AppCompatActivity {
         pending.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"Pending",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Pending", Toast.LENGTH_SHORT).show();
 
-                NotificationCompat.Builder notification = new NotificationCompat.Builder(MainActivity.this,Notification.EXTRA_CHANNEL_ID);
-                //Intent intent1 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Tony3898"));
-                Intent intent1 = new Intent(MainActivity.this,MainActivity.class);
-                PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this,1,intent1,PendingIntent.FLAG_CANCEL_CURRENT);
+                myNotificationChannel();
+                NotificationCompat.Builder notification = new NotificationCompat.Builder(MainActivity.this, myChannel);
+                Intent intent1 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Tony3898"));
+                PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 5, intent1, 0);
 
                 notification.setSmallIcon(R.drawable.ic_camera);
                 notification.setContentTitle("Tony");
@@ -69,17 +74,29 @@ public class MainActivity extends AppCompatActivity {
                 notification.setAutoCancel(true);
                 notification.setDefaults(Notification.DEFAULT_ALL);
 
-                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
+                /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
 
                     RemoteInput ri = new RemoteInput.Builder(myKey).setLabel("Replying..").build();
-                    NotificationCompat.Action action = new NotificationCompat.Action.Builder(R.drawable.ic_camera,"Reply..",pendingIntent).addRemoteInput(ri).build();
+                    NotificationCompat.Action action = new NotificationCompat.Action.Builder(R.drawable.ic_camera, "Reply..", pendingIntent).addRemoteInput(ri).build();
                     notification.addAction(action);
-                }
+                }*/
 
-                NotificationManager manager = (NotificationManager) getApplicationContext().getSystemService(NOTIFICATION_SERVICE);
-                manager.notify(1,notification.build());
+                manager.notify(1, notification.build());
 
             }
         });
+    }
+
+    public void myNotificationChannel() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String name = "My channel Name";
+            String desc = "My notification channel";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel notificationChannel = new NotificationChannel(myChannel, name, importance);
+            notificationChannel.setDescription(desc);
+            manager.createNotificationChannel(notificationChannel);
+
+        }
     }
 }
